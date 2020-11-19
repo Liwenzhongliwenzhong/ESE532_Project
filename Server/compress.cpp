b7dffe16d4a1047e03325650f5cb661e2ef7da18
Liwenzhongliwenzhong <70785541+Liwenzhongliwenzhong@users.noreply.github.com>
2020-11-19 10:05:18 -0500

    Update compress.cpp

--- Server/compress.cpp
+++ Server/compress.cpp
@@ -9,30 +9,10 @@ int compress(unsigned char* output, unsigned char* input, int length) {
 	while(i < length){
 		//split file into chunks
 		unsigned size = chunking(input+i, chunk, length-i);
-		//for each chunk, calculate SHA-256
-		// sha.update(chunk, size);
-		// uint8_t* digest = sha.digest();
-		// //deduplicate
-		// if (hash.SearchKey(digest) == NULL) {
-		// 	std::cout << "Match miss" << std::endl;
-		// 	hash.Insert(digest, chunk);
-		// 	lzw(chunk, output);
-		   
-		// } else {
-		//    std::cout << "Match found" << std::endl;
-		//    //write a 32b value
-		// }
 		i += size;
 		printf ("Chunk size = \t%d\n", size);
 		printf("First three characters of chunk:\t");
-		printf("%c", *chunk);
-		printf("%c", *(chunk+1));
-		printf("%c", *(chunk+2));
-		printf("\n");
-		// std::cout << "chunk size = " << size << '\t';
-		// std::cout << "Total size = " << i << std::endl;
-		// delete[] digest;
+		printf("%c%c%c\n", *chunk, *(chunk+1), *(chunk+1));
 	}
-
 	return 0;
 }
